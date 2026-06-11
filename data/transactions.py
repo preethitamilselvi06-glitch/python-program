cb06fb17e85b7ed379c864f573fb26748d5a15e0
preethitamilselvi06-glitch <preethitamilselvi06@gmail.com>
2026-06-11 10:39:55 +0530

    updated project files

--- data/transactions.py
+++ data/transactions.py
@@ -3,3 +3,7 @@ transactions =pd.read_csv(r"C:\Users\acer\Desktop\project\data\transactions.csv"
 print(transactions)
 print(transactions.info())
 print(transactions.shape)
+print(transactions.describe)
+print(transactions.head(20))
+print(transactions.isnull().sum())
+print(transactions.duplicated().sum())
