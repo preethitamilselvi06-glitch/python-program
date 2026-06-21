1b703f2b1e2cdf67f97351b28949917eca2c6327
preethitamilselvi06-glitch <preethitamilselvi06@gmail.com>
2026-06-21 21:06:12 +0530

    update

--- data/upi project work.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-dataset_table = pd.read_csv(r"C:\Users\acer\Desktop\project\data\upi_transactions_dataset.csv")
-print(dataset_table)
-print(dataset_table.info())
-print(dataset_table.shape)
